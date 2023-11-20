3537e9266736412a75b83c87fa4b0f866bc5437b
neddington <neddington@cascadia.edu>
2023-11-20 15:47:02 -0800

    Initial Commit

---
+++ Location/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  Location
+//
+//  Created by Eddington, Nick on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
